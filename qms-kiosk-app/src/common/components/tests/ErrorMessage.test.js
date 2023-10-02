82e18bb78efcf7ec4fb409332170e5d6c41eb956
Victor Trejo <vic3jo@gmail.com>
2023-10-02 08:13:34 -0400

    Add some tests and improvements

--- qms-kiosk-app/src/common/components/tests/ErrorMessage.test.js
+++ qms-kiosk-app/src/common/components/tests/ErrorMessage.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
 import ErrorMessage from '../ErrorMessage';
 
 jest.mock('react-i18next');
 
 describe('ErrorMessage component', () => {
   it('renders with no provided props as expected', () => {
-    const renderer = new ShallowRenderer();
-    const wrapper = renderer.render(<ErrorMessage />);
-    expect(wrapper).toMatchSnapshot();
+    expect(renderShallow(<ErrorMessage />)).toMatchSnapshot();
   });
 });
